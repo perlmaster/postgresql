861f33deb1dbb56b80f5a04e327a27dd8a18c752
Barry Kimelman <36140641+perlmaster@users.noreply.github.com>
2020-02-13 15:16:16 -0600

    Update postgres-list-db.pl

--- postgres-list-db.pl
+++ postgres-list-db.pl
@@ -22,10 +22,24 @@ use FindBin;
 use lib $FindBin::Bin;
 
 require "print_lists.pl";
+require "display_pod_help.pl";
+my %options = ( "d" => 0 , "h" => 0 );
 
 my ( $status , $dbh  , $database , $username , $password , $host );
 my ( $sql , $sth , $ref , $row , $port );
 my ( @Name , @Owner , @Encoding , @Collate , @Ctype , @Access_privileges );
+
+$status = getopts("hd",\%options);
+if ( $options{"h"} ) {
+	display_pod_help($0);
+	exit 0;
+} # IF
+unless ( $status && 2 < scalar @ARGV ) {
+	die("Usage : $0 [-dh] database username password [port]\n");
+} # UNLESS
+$host = "127.0.0.1";
+( $database , $username , $password ) = @ARGV;
+$port = (3 == scalar @ARGV) ? 5432 : $ARGV[3];
 
 @Name = ();
 @Owner = ();
@@ -34,11 +48,6 @@ my ( @Name , @Owner , @Encoding , @Collate , @Ctype , @Access_privileges );
 @Ctype = ();
 @Access_privileges = ();
 
-$database = 'mydatabase';
-$username = 'myusername';
-$password = 'mypassword';
-$host = "127.0.0.1";
-$port = 5432;
 
 $dbh = DBI->connect("dbi:Pg:dbname=$database;host=$host;port=$port",
                           $username,
@@ -90,3 +99,42 @@ print_lists( [ \@Name , \@Owner , \@Encoding , \@Collate , \@Ctype , \@Access_pr
 		"=");
 
 exit 0;
+__END__
+=head1 NAME
+
+postgres-list-db.pl - List the databases under a postgresql server
+
+=head1 SYNOPSIS
+
+postgres-list-db.pl [-dh] database username password [port]
+
+=head1 DESCRIPTION
+
+List the databases under a postgresql server
+
+=head1 PARAMETERS
+
+  database - name of a postgresql database
+  username - name of database user
+  password - password of database user
+  port - override default port number of 5432
+
+=head1 OPTIONS
+
+  -h - produce this summary
+  -d - activate debugging mode
+
+=head1 EXAMPLES
+
+postgres-list-db.pl mydb user123 pass123
+
+=head1 EXIT STATUS
+
+ 0 - successful completion
+ nonzero - an error occurred
+
+=head1 AUTHOR
+
+Barry Kimelman
+
+=cut
